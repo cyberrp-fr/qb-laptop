1be8e25f7ddfbb0b4c6555e03200aa06bcfd87af
0xIbra <ibragim.ai95@gmail.com>
2023-05-09 07:30:13 +0200

    terminal - ddos device creation script wip

--- vue/src/utils/linux/nodejs.ts
+++ vue/src/utils/linux/nodejs.ts
@@ -49,7 +49,7 @@ export default class NodeJS {
     }
 
     private _init() {
-        
+
     }
 
     private async _execjs(code: string) {
@@ -60,17 +60,45 @@ export default class NodeJS {
             consoleLog.apply(console, args);
         };
 
+        const CyberOrgAuthenticate = async (username, hash, version) => {
+            console.log("username: ", username)
+            console.log("hash: ", hash)
+            console.log("version: ", version)
 
+            console.log(' ')
+            console.log("[info] tentative connexion réseau cyber...")
+
+            const opts = {
+                method: "POST",
+                body: JSON.stringify({ username, hash, version }),
+                headers: {"Content-Type": "application/json"}
+            }
+            const response = await fetch("https://qb-laptop/ddosCyberOrgAuthenticate", opts)
+            if (response.status !== 200) {
+                console.log("[error] connexion échouée")
+
+                return false
+            }
+
+            const content = await response.json()
+
+            if (content.auth !== true) {
+                console.log("[error] connexion échouée")
+            } else if (content.auth === true) {
+                console.log("[info] connexion au réseau cyber réussie")
+            }
+
+            return content.auth === true
+        }
 
         ` + code
 
-        console.log("exec: ", code)
+        // console.log("exec: ", code)
 
         const blob = new Blob([code], { type: "application/javascript" })
         const worker = new Worker(URL.createObjectURL(blob))
 
         worker.onmessage = (e) => {
-            console.log('edata: ', e.data)
             this._outputCallback(e.data.message)
         }
     }
