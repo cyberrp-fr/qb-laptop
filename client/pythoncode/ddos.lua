40c0037152c8cb14faf5c550d7dffa8b2e7565dc
0xIbra <ibragim.ai95@gmail.com>
2023-05-09 15:54:29 +0200

    terminal - ddos - creation script improvement
    Making each step of the ddos device creation dependent on the previous one so as to make them purposeful and realistic.

--- client/pythoncode/ddos.lua
+++ client/pythoncode/ddos.lua
@@ -1,4 +1,5 @@
-
+local CyberOrgConnectionEstablished = false
+local DeviceConnectionEstablished = false
 
 -- CALLBACKS
 
@@ -7,11 +8,8 @@ RegisterNUICallback("ddosCyberOrgAuthenticate", function (data, cb)
     PlayerData = QBCore.Functions.GetPlayerData()
     if PlayerData.name == data.username and PlayerData.gang.iscyber == true then
         auth = true
+        CyberOrgConnectionEstablished = true
     end
-
-    -- print("req data: ", json.encode(data))
-    -- print("player data name: ", PlayerData.name)
-    -- print("player gang data: ", json.encode(PlayerData.gang))
 
     cb({ auth = auth })
 end)
@@ -20,11 +18,20 @@ RegisterNUICallback("ddosDeviceCheck", function (data, cb)
     local hash = data.hash
     if hash == nil then
         cb({ status = false, msg = "[error] le parametre \"hash\" est obligatoire." })
+        return
+    end
+
+    if not CyberOrgConnectionEstablished then
+        cb({ status = false, msg = "[error] aucune connexion réseau cyber trouvée, connectez vous au réseau d'abord."})
+        return
     end
 
     if not QBCore.Functions.HasItem(Config.Ddos.RequiredItem, 1) then
         cb({ status = false, msg = "[error] Aucun \"appareil programmable\" détecté." })
+        return
     end
+
+    DeviceConnectionEstablished = true
 
     cb({ status = true })
 end)
