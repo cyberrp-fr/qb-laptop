27a15376c4158c464153a17f1587f4796ad3e50f
CyberRP France <133641683+cyberrp-fr@users.noreply.github.com>
2024-06-30 20:19:11 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author '0xIbra - https://github.com/0xIbra'
+author 'CyberRP France - https://github.com/cyberrp-fr'
 description 'qb-laptop - laptops with linux OS'
 
 shared_scripts {
